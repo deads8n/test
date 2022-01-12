d934cba4d9eedb933287207791f4d0f22229427a
deads8n <69991788+deads8n@users.noreply.github.com>
2022-01-12 18:28:12 -0500

    Update headsortails.py

--- headsortails.py
+++ headsortails.py
@@ -3,9 +3,9 @@ import colorsys
 import time
 
 class colors:
-    green = '\033[92m'
-    yellow = '\033[93m'
-    red = '\033[91m'
+    green = "\033[92m"
+    yellow = "\033[93m"
+    red = "\033[91m"
 
 def whole():
     go = input("Ready to flip the coin? \"Y/N\"")
@@ -14,7 +14,7 @@ def whole():
         num = random.randint(1,2)
         if int(num) == 1:
             time.sleep(1)
-            print(colors.red + "You got -> " + colors.green + " HEADS")
+            print(colors.red + "You got ->" + colors.green + " HEADS")
         if int(num) == 2:
             time.sleep(1)
             print(colors.red + "You got -> " + colors.green + " TAILS")
